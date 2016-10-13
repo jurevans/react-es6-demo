5b4872472441613043b29f1860b5b579c5e84778
Justin Evans <jurevans@gmail.com>
2016-10-13 09:45:56 -0400

    Added tests. More to come.

--- src/jsx-pages/login/login.spec.js
+++ src/jsx-pages/login/login.spec.js
@@ -1,5 +1,5 @@
 describe('Test suite for Login page component', function() {
-	var component;														//adjust if firefly.jsx triggers more initial items
+	var component;
 
 	beforeAll(function() {
 
@@ -9,6 +9,21 @@ describe('Test suite for Login page component', function() {
 		);
 
 		component = reactTestUtils.renderIntoDocument(element);
+	});
+
+    /* Render */
+    it('The Login Page component renders to the page', function() {
+		console.log('The Login Page component should render without errors');
+
+		expect(function() {
+			component;
+		}).not.toThrow();
+	});
+
+    /* This component uses grandCentral */
+    it('Grand Central Exists', function(){
+		console.log("typeof GrandCentral = ", typeof grandCentral);
+		expect(typeof grandCentral).not.toBe("undefined")
 	});
 
 });
