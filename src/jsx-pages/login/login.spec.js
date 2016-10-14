5c91755c967abdbdaa95cce10fea8a591b8761df
Justin Evans <jurevans@gmail.com>
2016-10-14 10:09:41 -0400

    hrm...

--- src/jsx-pages/login/login.spec.js
+++ src/jsx-pages/login/login.spec.js
@@ -31,44 +31,28 @@ describe('Test suite for Login page component', function() {
 
     /* This component uses grandCentral to manage actions between fields, messages and Sign In button */
     it('Grand Central Exists', function(){
-		console.log("typeof GrandCentral = ", typeof grandCentral);
+		console.log("typeof GrandCentral = " + typeof grandCentral);
 
 		expect(typeof grandCentral).not.toBe("undefined")
 	});
 
-    describe('Sub-test - Username - check validity', function() {
+    describe('Sub-test - Fields - check validity', function() {
         beforeAll(function() {
-
-    	});
-
-        beforeEach(function(){
-
+            emailInput.value = SiteConfig.loginUsername;
+            passwordInput.value = SiteConfig.loginPassword;
     	});
 
         it('Should validate with the correct username', function(){
-    		/* ? */
-            emailInput.value = SiteConfig.loginUsername;
-
             console.log(emailInput.value);
 
-    	});
-    });
-
-    describe('Sub-test - Password - check validity', function() {
-        beforeAll(function() {
-
+            expect(emailInput.value).toBe(SiteConfig.loginUsername);
     	});
 
-        beforeEach(function(){
-
-    	});
-
-        it('Should validate with the correct password', function(){
-    		/* ? */
-            passwordInput.value = SiteConfig.loginPassword;
+        it('Should validate with the correct username', function(){
 
             console.log(passwordInput.value);
 
+            expect(passwordInput.value).toBe(SiteConfig.loginPassword);
     	});
     });
 
